ce6d22e1531efdda73de2b208d90423bb533f8fa
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2025-08-13 00:18:25 +0500

    Add project files.

---
+++ motive-integration-telegram/src/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Nodes;
+using System.Text.Json;
+
+namespace motive_integration_telegram.src
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            var apiKey = "";
+            var vehicleId = ;
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("accept", "application/json");
+            client.DefaultRequestHeaders.Add("x-api-key", apiKey);
+
+            var url = $"https://api.gomotive.com/v1/vehicle_locations?ids={vehicleId}";
+
+            try
+            {
+                var json = await client.GetStringAsync(url);
+
+                var doc = JsonNode.Parse(json);
+                var filteredVehicle = doc?["vehicles"]?
+                    .AsArray()
+                    .FirstOrDefault(v => v?["vehicle"]?["id"]?.ToString() == vehicleId.ToString());
+
+                Console.WriteLine(filteredVehicle?.ToJsonString(new JsonSerializerOptions { WriteIndented = true }));
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"HTTP error: {ex.Message}");
+            }
+        }
+    }
+}
