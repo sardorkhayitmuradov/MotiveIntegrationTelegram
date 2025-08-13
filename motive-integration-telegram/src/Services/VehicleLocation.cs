e5bf31f67c11ea54c10a3a6c40068f8cf0c3cbd0
khayitmuradov <khayitmuradov@proton.me>
2025-08-13 11:13:12 +0500

    Add VehicleLocation model for Motive API location response

---
+++ motive-integration-telegram/src/Services/VehicleLocation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace motive_integration_telegram.src.Services
+{
+    public class VehicleLocation
+    {
+        public string VehicleId { get; set; } = "";
+        public double Latitude { get; set; }
+        public double Longtitude { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
