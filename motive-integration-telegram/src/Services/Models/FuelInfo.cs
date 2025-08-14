e1d50e23f03556ba58a5bb7783f89fd3e1e4fc9d
khayitmuradov <khayitmuradov@proton.me>
2025-08-14 06:51:16 +0500

    feat: Add FuelInfo

---
+++ motive-integration-telegram/src/Services/Models/FuelInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace motive_integration_telegram.src.Services.Models
+{
+    public class FuelInfo
+    {
+        public string VehicleId { get; set; } = "";
+        public double FuelPercentage { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
