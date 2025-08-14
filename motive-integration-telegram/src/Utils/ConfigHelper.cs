4e65b51d471456c3fc06b187ead475acdeb38771
khayitmuradov <khayitmuradov@proton.me>
2025-08-14 07:07:30 +0500

    feat: Add ConfigHelper

---
+++ motive-integration-telegram/src/Utils/ConfigHelper.cs
@@ -0,0 +1,42 @@
+﻿using DotNetEnv;
+
+namespace motive_integration_telegram.src.Utils
+{
+    public static class ConfigHelper
+    {
+        private static bool _isLoaded = false;
+
+        private static void EnsureLoaded()
+        {
+            if (!_isLoaded)
+            {
+                try
+                {
+                    Env.Load();
+                    _isLoaded = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Could not load .env file: {ex.Message}");
+                }
+            }
+        }
+
+        public static string GetEnvVar(string key, string? defaultValue = null)
+        {
+            EnsureLoaded();
+
+            var value = Environment.GetEnvironmentVariable(key);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                if (defaultValue != null)
+                    return defaultValue;
+
+                throw new NotImplementedException($"Environment variable '{key}' is missing.");
+            }
+
+            return value;
+        }
+    }
+}
